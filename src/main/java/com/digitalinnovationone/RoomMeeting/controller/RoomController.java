4ba95862538b80354313ab989cff830ff19efa4d
GabrielFaria23 <gabrielfariapta01@gmail.com>
2020-12-16 10:54:34 -0300

    RoomController implemented

--- src/main/java/com/digitalinnovationone/RoomMeeting/controller/RoomController.java
+++ src/main/java/com/digitalinnovationone/RoomMeeting/controller/RoomController.java
@@ -1,11 +1,17 @@
 package com.digitalinnovationone.RoomMeeting.controller;
 
+import com.digitalinnovationone.RoomMeeting.exception.ResourceNotFoundException;
+import com.digitalinnovationone.RoomMeeting.model.Room;
 import com.digitalinnovationone.RoomMeeting.repository.RoomRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.CrossOrigin; //permite que o front consuma nossa api backend.
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
@@ -15,5 +21,45 @@ public class RoomController {
     @Autowired
     private RoomRepository roomRepository;
 
-    
+    @GetMapping("/rooms")
+    public List<Room> getAllRooms(){
+        return roomRepository.findAll();
+    }
+
+    @GetMapping("/rooms/{id}")
+    public ResponseEntity<Room> getRoomById(@PathVariable long id) throws ResourceNotFoundException {
+            Room room = verifyRoomExists(id, "Room not found with id: ");
+            return ResponseEntity.ok().body(room);
+    }
+
+    @PostMapping("/rooms")
+    public Room createRoom(@Validated @RequestBody Room room){
+        return roomRepository.save(room);
+    }
+
+    @PutMapping("/rooms/{id}")
+    public ResponseEntity<Room> updateRoom(@PathVariable long id, @Validated @RequestBody Room room) throws ResourceNotFoundException {
+        Room roomToUpdate = verifyRoomExists(id, "Room not found with id: ");
+        roomToUpdate.setName(room.getName());
+        roomToUpdate.setDate(room.getDate());
+        roomToUpdate.setStartHour(room.getStartHour());
+        roomToUpdate.setEndHour(room.getEndHour());
+        final Room updateRoom = roomRepository.save(roomToUpdate);
+        return ResponseEntity.ok(updateRoom);
+    }
+
+    @DeleteMapping("/rooms/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public Map<String, Boolean> deleteRoom(@PathVariable long id) throws ResourceNotFoundException {
+        Room roomToDelete = verifyRoomExists(id, "Room not found with id: ");
+        roomRepository.delete(roomToDelete);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", Boolean.TRUE);
+        return response;
+    }
+
+    private Room verifyRoomExists(@PathVariable long id, String s) throws ResourceNotFoundException {
+        return roomRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException(s + id));
+    }
 }
--- src/main/java/com/digitalinnovationone/RoomMeeting/exception/GlobalExceptionHandler.java
+++ src/main/java/com/digitalinnovationone/RoomMeeting/exception/GlobalExceptionHandler.java
@@ -12,7 +12,7 @@ import java.util.Date;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler (ResourceNotFoundException.class)
-    public ResponseEntity<?> resourseNotFoundException(ResourceNotFoundException ex, WebRequest request){
+    public ResponseEntity<?> resourceNotFoundException(ResourceNotFoundException ex, WebRequest request){
         ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
         return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
     }
